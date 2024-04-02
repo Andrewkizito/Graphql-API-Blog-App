c30bc6eda6cf4e0d8404177b9e30374886835807
KIzito Andrew <Andrewkizito54@gmail.Com>
2024-04-02 10:14:32 +0300

    feat: improved tables

--- src/config/db_tables.mjs
+++ src/config/db_tables.mjs
@@ -2,21 +2,21 @@ const UsersTable = `
     CREATE TABLE IF NOT EXISTS users (
         id SERIAL,
         username VARCHAR(20) UNIQUE NOT NULL,
-        password VARCHAR(32) NOT NULL,
+        password VARCHAR(64) NOT NULL,
         email VARCHAR(50) UNIQUE NOT NULL,
         PRIMARY KEY(id)
     )
 `
 
 const PostsTable = `
-    CREATE TABLE IF NOT EXISTS Posts (
+    CREATE TABLE IF NOT EXISTS posts (
         id SERIAL,
-        author INT,
         title VARCHAR(120) NOT NULL,
         slug VARCHAR(200) NOT NULL UNIQUE,
         description VARCHAR(200) NOT NULL,
-        cover_photo VARCHAR(80) NOT NULL,
+        cover_photo VARCHAR(80),
         content TEXT NOT NULL,
+        author INT NOT NULL,
         PRIMARY KEY(id),
         CONSTRAINT fk_author_id FOREIGN KEY(author) REFERENCES users(id)
     )
