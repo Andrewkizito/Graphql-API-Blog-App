9aea21102a6d129ba0b6d110dc30bb01611e207e
KIzito Andrew <Andrewkizito54@gmail.Com>
2024-04-02 11:56:27 +0300

    feat: added more env varibles

--- src/config/env.mjs
+++ src/config/env.mjs
@@ -5,7 +5,9 @@ dotenv.config()
 
 const env = {
     DB_URI: process.env.DB_URI,
-    PORT: process.env.PORT
+    PORT: process.env.PORT,
+    NODE_ENV: process.env.NODE_ENV,
+    JWT_SECRET: process.env.JWT_SECRET
 }
 
 export default env
