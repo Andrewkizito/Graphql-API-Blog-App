5f52d7682d50bb25e92374b827420cef21df479c
KIzito Andrew <Andrewkizito54@gmail.Com>
2024-04-02 11:58:09 +0300

    feat: added user authenticator

--- src/utils/auth.mjs
+++ src/utils/auth.mjs
@@ -1,10 +1,10 @@
 // DB Utils
 import { db } from '../config/database.mjs';
-import { CREATE_USER } from '../config/queries/users.mjs';
+import { CREATE_USER, FIND_USER_BY_EMAIL_OR_USERNAME } from '../config/queries/users.mjs';
 
 // Validation
-import { userSchema } from '../validators/auth.mjs';
-import { createHashFromString, validateData } from './common.mjs';
+import { userLoginSchema, userSchema } from '../validators/auth.mjs';
+import { compareHashes, createHashFromString, generateJWTToken, validateData } from './common.mjs';
 
 
 async function addUserToDB(params) {
@@ -12,29 +12,52 @@ async function addUserToDB(params) {
         await db.oneOrNone(CREATE_USER, [params.username, params.email, params.password])
     } catch (error) {
         if (error?.code === '23505') {
-            throw new Error(error?.detail.includes("username") 
-            ? `Username - ${params.username} is already taken` 
-            : `Email - ${params.email} is already registered`)
+            throw new Error(error?.detail.includes("username")
+                ? `Username - ${params.username} is already taken`
+                : `Email - ${params.email} is already registered`)
         }
         throw new Error(error.message)
     }
 }
 
+async function findUser(identifier) {
+    try {
+        const user = await db.oneOrNone(FIND_USER_BY_EMAIL_OR_USERNAME, identifier)
+        if (!user) throw new Error("Incorrect account details provided")
+
+        return user
+    } catch (error) {
+        console.log(error)
+        throw new Error(error.message)
+    }
+}
+
 async function createUser(params) {
-    // Validate params
     const result = validateData(params, userSchema)
 
-    if (result.error) {
-        throw new Error(result.error)
-    } else if (result.value) {
-        const payload = {
-            ...result.value,
-            password: createHashFromString(result.value.password)
-        }
-
-        await addUserToDB(payload)
+    const payload = {
+        ...result.value,
+        password: createHashFromString(result.value.password)
     }
+
+    await addUserToDB(payload)
 
 }
 
-export { createUser }
+async function authenticateUser(params) {
+    const result = validateData(params, userLoginSchema)
+
+    const user = await findUser(result.value.identifier)
+
+    const isPasswordCorrect = compareHashes(user.password, result.value.password)
+
+    if (!isPasswordCorrect) {
+        throw new Error("Incorrect account details provided")
+    }
+
+    return {
+        jwt: generateJWTToken({ data: user.username })
+    }
+}
+
+export { createUser, authenticateUser }
