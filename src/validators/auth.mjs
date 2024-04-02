cdb20a5c20c1a252c3fa4c42358d3111094b9876
KIzito Andrew <Andrewkizito54@gmail.Com>
2024-04-02 10:57:04 +0300

    feal cleanup

--- src/validators/auth.mjs
+++ src/validators/auth.mjs
@@ -1,9 +1,9 @@
 import joi from 'joi'
 
-const user = joi.object({
+const userSchema = joi.object({
     username: joi.string().alphanum().min(3).max(20).required(),
     email: joi.string().email().max(64).required(),
     password: joi.string().min(6).max(20).required()
 })
 
-export { user }
+export { userSchema }
